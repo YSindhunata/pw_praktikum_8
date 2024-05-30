f0c5834a74ed976d85b84b1e8b3511e8c9da797a
YSindhunata <ysindhunata@gmail.com>
2024-05-30 17:14:40 +0700

    Praktikum 8 Vue

---
+++ vue-project-praktikum8/src/components/ArtikelHome.vue
@@ -0,0 +1,10 @@
+<template>  
+<div>
+    <img src="../assets/Orang.gif" alt="">
+    <h1>Ini adalah komponen artikel hoome</h1>
+    <p>Ini adalah isi artikel home</p>
+    <p>Ini adalah isi artikel home</p>
+    <p>Ini adalah isi artikel home</p>
+    <p>Ini adalah isi artikel home</p>
+</div>
+</template>
